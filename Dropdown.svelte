55d6f9b32f6e5351f45148adbda55c0721c28f99
Astro-Ramesh <98573553+Astro-Ramesh@users.noreply.github.com>
2022-03-07 19:10:26 +0530

    Update Dropdown.svelte

--- Dropdown.svelte
+++ Dropdown.svelte
@@ -17,5 +17,6 @@
     </select>
         <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
-          </svg> -->
+          </svg>
+ class="flex flex-col w-full md:w-64 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0"-->
   </div>
